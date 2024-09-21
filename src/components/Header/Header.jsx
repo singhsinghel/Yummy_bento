2959552a2b41a00949148f89a15d1912ccae1622
ankitsinghsinghel <singhelboyankit@gmail.com>
2024-09-21 19:17:41 +0530

    header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents d-flex flex-column align-items-start gap-4">
-        <h2 className='fw-bold  text-light'>Order your favourite food here</h2>
+        <h2 className='fw-bold text-light'>Order your favourite food here</h2>
         <p className='text-white d-md-inline d-none  fs-6'> Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.our mission is to satisfy your cravings and elevate your dininig experience, one delicious meal at a time. </p>
         <button className='btn btn-light px-3 fw-bold rounded-5' style={{color:'#747474'}}> View menu</button>
       </div>
