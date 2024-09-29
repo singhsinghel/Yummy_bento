ef57ef0bbc6dceb98699f47c8d97d32cb3e446d3
ankitsinghsinghel <singhelboyankit@gmail.com>
2024-09-29 16:20:18 +0530

    cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 import {assets} from '../../assets/frontend_assets/assets'
 import Button from '@mui/material/Button';
 import SignIn from '../SignIn/SignIn';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -10,7 +11,7 @@ const Navbar = () => {
   return (
     <>
     <div className='navbar sticky-top px-3 py-2 p-md-3 d-flex justify-content-between align-items-center w-100'>
-      <h2 className='h4'><span className='logo'>Yummy</span><span className='text-secondary'>Bento</span></h2>
+      <Link to='/' className='h4'><span className='logo'>Yummy</span><span className='text-secondary'>Bento</span></Link>
       <ul className='navbar-menu d-none d-sm-flex align-items-end list-unstyled gap-3 gap-md-4 fs-6 cursor-pointer'style={{color:'#49557e'}}>
         <a href='#app' onClick={()=>setMenu('home')} className={menu==='home'?'active':''}>Home</a>
         <a href='#explore-menu' onClick={()=>setMenu('menu')} className={menu==='menu'?'active':''}>menu</a>
@@ -20,7 +21,7 @@ const Navbar = () => {
       <div className="navbar-right d-flex align-items-center jutify-content-between gap-3 gap-md-4">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon position-relative ">
-          <img src={assets.basket_icon} alt="" />
+         <Link to='/cart'> <img className='cursor-pointer' src={assets.basket_icon} alt="" /></Link>
           <div className="dot"> </div>
         </div>
         <SignIn  />
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {StoreContext} from '../../context/Context'
 import './Cart.css'
 const Cart = () => {
+  const {cartItems,food_list,removeItem} = useContext(StoreContext)
   return (
     <div>
-      
-    </div>
+      <div className="cart-items">
+        <div className="cart-items-title row text-center">
+        <p className='col-2'>Items</p>
+        <p className='col-2'>Title</p>
+        <p className='col-2'>Price</p>
+        <p className='col-2'>Quantity</p>
+        <p className='col-2'>Total</p>
+        <p className='col-2'>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id]>0){
+            return(
+              <div key={index} className='cart-items-title cart-items-item row mt-3 text-center'>
+                <div className="img col-2">
+                  <img style={{width:'4rem'}}  src={item.image} alt="" />
+                  </div>
+                  <p className='col-2'>{item.name}</p>
+                  <p className='col-2'>{item.price}</p>
+                  <p className='col-2'>{cartItems[item._id]}</p>
+                  <p className='col-2'>$ {item.price*cartItems[item._id]}</p>
+                  <div className="delete col-2 text-center">
+                  <p onClick={()=>removeItem(item._id)} className='text-danger fw-bolder cursor-pointer'>X</p>
+                  </div>
+              </div>
+            )
+          }
+        })}
+        </div>
+        <div className="checkout-coupon d-flex justify-content-between">
+          <div className="totals">
+            <h2>Cart Totals</h2>
+            <div className="subtotal">
+              <p>Subtotal</p>
+              <p>{}</p>
+            </div>
+            <hr />
+            <div className="deliv-fee">
+              <p>Delivery Fee</p>
+              <p>{}</p>
+            </div>
+            <hr />
+            <div className="total">
+              <p>Total</p>
+              <p>{}</p>
+            </div>
+            <div style={{backgroundColor:'tomato'}} className="btn text-light">Checkout</div>
+          </div>
+          <div className="coupon ">
+            <p>If you have any promo code, Enter here</p>
+            <form action="">
+              <div className="row ">
+                <div className="input col-8 p-0">
+                 <input className=' form-control rounded-start-5' type="text" />
+              </div>
+              <button className="btn btn-dark col-4">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
   )
 }
 
