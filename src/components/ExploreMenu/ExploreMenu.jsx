62ba73b1669c795e3e4173260addd1b92dbc2ab7
ankitsinghsinghel <singhelboyankit@gmail.com>
2024-09-30 12:34:53 +0530

    simplified animation

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -3,7 +3,7 @@ import './ExploreMenu.css'
 import { menu_list } from '../../assets/frontend_assets/assets'
 const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div className='explore-menu d-flex flex-column gap-4 mt-4' id='explore-menu'>
+    <div className='explore-menu d-flex flex-column gap-4' id='explore-menu'>
       <h1 className='h1 fw-bold'style={{color:'#262626'}}>Explore our menu</h1>
       <p className='explore-menu-text mw-60' style={{color:'#262626'}} >Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.our mission is to satisfy your cravings and elevate your dininig experience, one delicious meal at a time.</p>
       <div className="explore-menu-list d-flex justify-content-between align-items-center gap-4 gap-sm-5 text-center mx-6" style={{overflowX:'scroll'}}>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,85 @@
-import React, { useContext, useState } from 'react'
-import './Navbar.css'
-import {StoreContext} from '../../context/Context'
+import React, { useContext, useEffect, useState } from 'react';
+import './Navbar.css';
+import { StoreContext } from '../../context/Context';
 import SignIn from '../SignIn/SignIn';
 import { Link, useLocation } from 'react-router-dom';
+import { gsap } from 'gsap';
+
 const Navbar = () => {
+  const isMediumOrLarger = window.matchMedia("(min-width: 768px)").matches;
+  
   const location = useLocation();
   const currentRoute = location.pathname;
-  const color=currentRoute==='/'?'white':'#49557e';
-  const bgColor=currentRoute==='/'?'transparent':'white'
-  const {getTotalCartAmt} = useContext(StoreContext)
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const[menu,setMenu]= useState('menu')
+  const position = (currentRoute === '/' && isMediumOrLarger) ? '' : 'sticky-top';
+  const color = (currentRoute === '/' && isMediumOrLarger) ? 'white' : '#49557e';
+  const bgColor = currentRoute==='/' ? 'transparent' : 'white';
+  const { getTotalCartAmt } = useContext(StoreContext);
+  const [menu, setMenu] = useState('menu');
+
+  useEffect(() => {
+    if (currentRoute==='/'&& isMediumOrLarger) { // Only run the animations on the home page
+      const tl = gsap.timeline();
+
+      tl.from('.cover-img', {
+        opacity: 0,
+        y: -500,
+        duration: 1,
+        delay: 0.5,
+        ease: 'power3.out',
+      })
+      .from('.h4', {
+        opacity: 0,
+        y: -50,
+        duration: 1,
+        ease: 'power2.out',
+      })
+      .from('.navbar-menu', {
+        opacity: 0,
+        y: -50,
+        duration: 1,
+        ease: 'power2.out',
+      }, '-=0.5') // Overlap with the previous animation by 0.5 seconds
+      .from('.navbar-right', {
+        opacity: 0,
+        y: -50,
+        duration: 1,
+        ease: 'power2.out',
+      }, '-=0.5')
+      .from('.inp-btn', {
+        opacity: 0,
+        y:40,
+        duration: 1,
+        ease: 'power4.out',
+      })
+    }
+  }, [currentRoute==='/']);
+
   return (
-    <>
-    <div className='navbar sticky-top px-3 py-2 p-md-3 d-flex justify-content-between align-items-center w-100' style={{backgroundColor:bgColor}}>
-      <Link to='/' className='h4'><span className='logo'>Yummy</span><span className='span' style={{color:color}}>Bento</span></Link>
-      <ul className='navbar-menu d-none d-sm-flex align-items-end list-unstyled gap-3 gap-md-4 fs-6 cursor-pointer'style={{color:color}}>
-        <a href='#app' onClick={()=>setMenu('home')} className={menu==='home'?'active':''}>Home</a>
-       
-        <a href='#explore-menu' onClick={()=>setMenu('menu')} className={menu==='menu'?'active':''}>menu</a>
-        <a href='#app-download' onClick={()=>setMenu('mobile-app')} className={menu==='mobile-app'?'active':''}>mobile-app</a>
-        <a href='#footer' onClick={()=>setMenu('contact-us')} className={menu==='contact-us'?'active':''}>Contact Us</a>
+    <div className={`navbar px-3 py-2 p-md-3 d-flex justify-content-between align-items-center w-100 ${position}`} style={{ backgroundColor: bgColor,zIndex:99999999 }}>
+      <Link to='/' className='h4'>
+        <span className='logo'>Yummy</span>
+        <span className={`text-${position==='sticky-top'?'secondary':'light'}`}>Bento</span>
+        <span className='fw-bold fs-4 logo'>.</span>
+      </Link>
+   
+      <ul className='navbar-menu d-none d-sm-flex align-items-end list-unstyled gap-3 gap-md-4 fs-6 cursor-pointer' style={{ color }}>
+        <a href='#app' onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ''}>Home</a>
+        <a href='#explore-menu' onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''}>Menu</a>
+        <a href='#app-download' onClick={() => setMenu('mobile-app')} className={menu === 'mobile-app' ? 'active' : ''}>Mobile App</a>
+        <a href='#footer' onClick={() => setMenu('contact-us')} className={menu === 'contact-us' ? 'active' : ''}>Contact Us</a>
       </ul>
-      <div className="navbar-right d-flex align-items-center jutify-content-between gap-3 gap-md-4">
-      <i className='bx bx-search-alt search' style={{transform:"scale(1.5)",color:color}} ></i>
-        <div className="navbar-search-icon position-relative ">
-         <Link to='/cart'> <i className='bx bx-basket basket'style={{transform:"scale(1.5)",color:color}} ></i></Link>
-          {getTotalCartAmt()>0&& <div className="dot"> </div>}
+      <div className="navbar-right d-flex align-items-center justify-content-between gap-3 gap-md-4">
+        <i className='bx bx-search-alt' style={{ transform: "scale(1.5)", color }}></i>
+        <div className="navbar-search-icon position-relative">
+          <Link to='/cart'>
+            <i className='bx bx-basket' style={{ transform: "scale(1.5)", color }}></i>
+          </Link>
+          {getTotalCartAmt() > 0 && <div className="dot"></div>}
         </div>
-        <SignIn  />
+        <SignIn />
       </div>
     </div>
-    </>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
